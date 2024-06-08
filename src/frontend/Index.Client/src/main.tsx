e7e21b2f61e1548f9a2fb6ef6b0d87f6d44c657b
Jathavaan Shankarr <jathavaan12@gmail.com>
2024-06-09 00:39:18 +0200

    IND-8 Created navbar

--- src/frontend/Index.Client/src/main.tsx
+++ src/frontend/Index.Client/src/main.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { createTheme, ThemeProvider } from "@mui/material";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+let theme = createTheme({
+  components: {
+    MuiAppBar: {
+      styleOverrides: {
+        root: {
+          backgroundColor: "#454f55",
+        },
+      },
+    },
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <ThemeProvider theme={theme}>
     <App />
-  </React.StrictMode>,
-)
+  </ThemeProvider>,
+);
---
+++ src/frontend/Index.Client/src/modules/core/components/NavigationBar.tsx
@@ -0,0 +1,16 @@
+﻿import { AppBar, Grid, Toolbar, Typography } from "@mui/material";
+export default function NavigationBar() {
+  return (
+    <AppBar sx={{}}>
+      <Toolbar>
+        <Grid container>
+          <Grid item xs={3}>
+            <Typography variant="h4">.index</Typography>
+          </Grid>
+          <Grid item xs={6}></Grid>
+          <Grid item xs={3}></Grid>
+        </Grid>
+      </Toolbar>
+    </AppBar>
+  );
+}
